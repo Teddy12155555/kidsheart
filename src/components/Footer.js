f14b15c243882e99dfd57c9f2a37790b27c86f65
ODD2 <vhanxtreme@gmail.com>
2020-04-08 18:45:15 +0800

    Home and Footer partial complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,35 @@
 import React, { useState, Component } from "react";
+import "../scss/footer.scss";
 
 export default function Footer(props) {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="seperator" />
+      <div className="container">
+        <div className="icons"></div>
+        <div className="column">
+          <div className="title">HOME</div>
+          <div className="element">首頁</div>
+          <div className="element">關於我們</div>
+          <div className="element">服務項目</div>
+          <div className="element">成果展示</div>
+          <div className="element">活動項目</div>
+        </div>
+
+        <div className="column">
+          <div className="title">LOCATION</div>
+          <div className="element">公司地址</div>
+        </div>
+
+        <div className="column">
+          <div className="title">HELP</div>
+          <div className="element">聯絡我們</div>
+        </div>
+
+        <div className="column">
+          <div className="title">SPONSOR</div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,39 @@
 import React, { useState, Component } from "react";
+import { Slide } from "react-slideshow-image";
+import "../scss/home.scss";
+
+const slideImages = [
+  "/assets/Image/slider0.jpg",
+  "/assets/Image/slider1.jpg",
+  "/assets/Image/slider2.jpg",
+];
+
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true,
+  pauseOnHover: true,
+  onChange: (oldIndex, newIndex) => {
+    console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+  },
+};
 
 export default function HomePage(props) {
-  return <div>HomePage</div>;
+  return (
+    <div className="homePage">
+      <div className="slide-container">
+        <Slide {...properties}>
+          {slideImages.map((v, i) => {
+            return (
+              <div className="each-slide" key={i}>
+                <img src={v} className="photo"></img>
+              </div>
+            );
+          })}
+        </Slide>
+      </div>
+    </div>
+  );
 }
