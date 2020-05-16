950d7df23bc67bf4337096e1bbd474358d0f69fe
OD <vhanxtreme@gmail.com>
2020-05-16 15:52:07 +0800

    Added Emial Reply in Contact Page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,36 @@
 import React, { useState, Component } from "react";
 import "../scss/contact.scss";
+import emailjs from "emailjs-com";
 
 export default function Contact(props) {
   const [Send, setSend] = useState("送出");
+  // const Send = "送出";
   const SendEmail = () => {
-    setSend("已送出！謝謝您！");
+    console.log("Sending");
+    var templateParams = {
+      to_email: "vhanxtreme@gmail.com",
+      message: document.getElementById("reply_textbox").value,
+    };
+
+    emailjs
+      .send(
+        "outusing",
+        "childsite_template_666",
+        templateParams,
+        "user_vpUEEw0pcyzGOLrYRqK1j"
+      )
+      .then(
+        function (response) {
+          console.log("SUCCESS!", response.status, response.text);
+          alert("感謝您的回覆!");
+          window.location.reload();
+        },
+        function (error) {
+          alert("回覆系統無法正常運作，請稍後再嘗試。");
+        }
+      );
+    console.log("GG");
+    // setSend("已送出！謝謝您！");
   };
   return (
     <div className="contact">
@@ -30,10 +56,11 @@ export default function Contact(props) {
         </div>
         <div className="right">
           <div className="title">給童心匯一點建議或鼓勵</div>
-          <form>
-            <textarea name="message" />
+          <div className="form">
+            <textarea name="message" id="reply_textbox" />
             <button onClick={SendEmail}>{Send}</button>
-          </form>
+          </div>
+
           <div className="title-2">您的寶貴意見</div>
           <div className="title-2">是我們進步的助力！</div>
         </div>
